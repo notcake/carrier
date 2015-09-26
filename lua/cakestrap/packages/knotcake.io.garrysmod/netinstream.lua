e15774800166883b2c12aa6b06e723089b91cf98
notcake <cakenotfound@gmail.com>
2015-09-26 01:41:04 +0100

    Fixed position in NetInStream not being updated.

--- lua/cakestrap/packages/knotcake.io.garrysmod/netinstream.lua
+++ lua/cakestrap/packages/knotcake.io.garrysmod/netinstream.lua
@@ -25,49 +25,58 @@ end
 
 -- IInStream
 function self:Read (size)
+	self.Position = self.Position + size
 	return net.ReadData (size)
 end
 
 -- IStreamReader
-function self:UInt8    () return net.ReadUInt ( 8) end
-function self:UInt8LE  () return net.ReadUInt ( 8) end
-function self:UInt8BE  () return net.ReadUInt ( 8) end
+function self:UInt8    () self.Position = self.Position + 1 return net.ReadUInt ( 8) end
+function self:UInt8LE  () self.Position = self.Position + 1 return net.ReadUInt ( 8) end
+function self:UInt8BE  () self.Position = self.Position + 1 return net.ReadUInt ( 8) end
 
-function self:Int8     () return net.ReadInt  ( 8) end
-function self:Int8LE   () return net.ReadInt  ( 8) end
-function self:Int8BE   () return net.ReadInt  ( 8) end
+function self:Int8     () self.Position = self.Position + 1 return net.ReadInt  ( 8) end
+function self:Int8LE   () self.Position = self.Position + 1 return net.ReadInt  ( 8) end
+function self:Int8BE   () self.Position = self.Position + 1 return net.ReadInt  ( 8) end
 
-function self:UInt16LE () return net.ReadUInt (16) end
-function self:Int16LE  () return net.ReadInt  (16) end
+function self:UInt16LE () self.Position = self.Position + 2 return net.ReadUInt (16) end
+function self:Int16LE  () self.Position = self.Position + 2 return net.ReadInt  (16) end
 
-function self:UInt32LE () return net.ReadUInt (32) end
-function self:Int32LE  () return net.ReadInt  (32) end
+function self:UInt32LE () self.Position = self.Position + 4 return net.ReadUInt (32) end
+function self:Int32LE  () self.Position = self.Position + 4 return net.ReadInt  (32) end
 
-function self:FloatLE  () return net.ReadFloat  () end
-function self:DoubleLE () return net.ReadDouble () end
+function self:FloatLE  () self.Position = self.Position + 4 return net.ReadFloat  () end
+function self:DoubleLE () self.Position = self.Position + 8 return net.ReadDouble () end
 
-function self:Char     () return net.ReadData ( 1) end
+function self:Char     () self.Position = self.Position + 1 return net.ReadData ( 1) end
 
 -- StreamReader
 function self:UInt81 ()
+	self.Position = self.Position + 1
+	
 	local uint8 = net.ReadUInt (8)
 	local uint80 = GarrysMod.BitConverter.UInt8ToUInt8s (uint8)
 	return uint80
 end
 
 function self:UInt82 ()
+	self.Position = self.Position + 2
+	
 	local uint16 = net.ReadUInt (16)
 	local uint80, uint81 = GarrysMod.BitConverter.UInt16ToUInt8s (uint16)
 	return uint80, uint81
 end
 
 function self:UInt84 ()
+	self.Position = self.Position + 4
+	
 	local uint32 = net.ReadUInt (32)
 	local uint80, uint81, uint82, uint83 = GarrysMod.BitConverter.UInt32ToUInt8s (uint32)
 	return uint80, uint81, uint82, uint83
 end
 
 function self:UInt88 ()
+	self.Position = self.Position + 8
+	
 	local uint80, uint81, uint82, uint83 = self:UInt84 ()
 	local uint84, uint85, uint86, uint87 = self:UInt84 ()
 	return uint80, uint81, uint82, uint83, uint84, uint85, uint86, uint87
