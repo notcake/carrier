58a4794c0737ef9c96df5b78fbb722a6b4faf428
notcake <cakenotfound@googlemail.com>
2017-07-07 00:19:51 +0100

    Cleaned up View:InstallPanelEventHandler ().

--- lua/carrier/packages/phoenix.garrysmod/view.lua
+++ lua/carrier/packages/phoenix.garrysmod/view.lua
@@ -175,17 +175,9 @@ end
 
 local methodTable = self
 function self:InstallPanelEventHandler (panelMethodName, methodName, handler)
-	local defaultMethod = self.Panel [panelMethodName]
+	if self [methodName] == methodTable [methodName] then return end
 	
-	if self [methodName] == methodTable [methodName] and
-	   defaultMethod then
-		self.Panel [panelMethodName] = function (_, ...)
-			defaultMethod (self.Panel, ...)
-			handler (...)
-		end
-	else
-		self.Panel [panelMethodName] = function (_, ...)
-			handler (...)
-		end
+	self.Panel [panelMethodName] = function (_, ...)
+		handler (...)
 	end
 end
