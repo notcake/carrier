a3470b45ac80fbbb6b3af0e48fb771ddd4aea354
notcake <cakenotfound@googlemail.com>
2017-07-18 00:48:15 +0100

    Added Async library.

---
+++ lua/carrier/packages/pylon.async.garrysmod.lua
@@ -0,0 +1,51 @@
+local CompactList = require ("Pylon.Containers.CompactList")
+local Future      = require ("Pylon.Future")
+local Clock       = require_provider ("Pylon.MonotonicClock")
+
+local Async = {}
+
+function Async.Wait (delay)
+	local future = Future ()
+	timer.Simple (delay, function () future:Resolve () end)
+	return future
+end
+
+Async.Sleep = Async.Wait
+
+-- WaitTick
+local tickWaiterCount, tickWaiters = CompactList.Clear()
+function Async.WaitTick ()
+	local future = Future ()
+	tickWaiterCount, tickWaiters = CompactList.Append(tickWaiterCount, tickWaiters, future)
+	return future
+end
+
+hook.Add (MENU_DLL and "Think" or "Tick", "async",
+	function ()
+		local previousTickWaiterCount, previousTickWaiters = tickWaiterCount, tickWaiters
+		tickWaiterCount, tickWaiters = CompactList.Clear ()
+		for f in CompactList.Enumerator (previousTickWaiterCount, previousTickWaiters) do
+			f:Resolve ()
+		end
+	end
+)
+
+-- AwaitQuota
+local quotaStartTimes = {}
+function Async.AwaitQuota (maximumDuration)
+	local maximumDuration = maximumDuration or 0.0025 -- s
+	local thread = coroutine.running ()
+	local t0 = quotaStartTimes [thread] or -math.huge
+	if Clock () - t0 > maximumDuration then
+		async.waitTick():wait(
+			function ()
+				quotaStartTimes [thread] = Clock ()
+				coroutine.resume (thread)
+				quotaStartTimes [thread] = nil
+			end
+		)
+		coroutine.yield ()
+	end
+end
+
+return Async
