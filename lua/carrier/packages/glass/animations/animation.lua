fee6722fc59d37164ccf2309dee7f22482b091ad
notcake <cakenotfound@googlemail.com>
2017-08-07 19:54:47 +0100

    Improved animation API a little.

--- lua/carrier/packages/glass/animations/animation.lua
+++ lua/carrier/packages/glass/animations/animation.lua
@@ -4,6 +4,8 @@ Glass.Animation = Class (self, Glass.IAnimation)
 function self:ctor (t0, updater, ...)
 	self.StartTime = t0
 	
+	self.Completed = false
+	
 	self.Updater = updater
 	
 	self.ArgumentCount, self.Arguments = CompactList.Pack (...)
@@ -14,7 +16,16 @@ function self:GetStartTime ()
 	return self.StartTime
 end
 
+function self:IsCompleted ()
+	return self.Completed
+end
+
 function self:Update (t)
 	local uncompleted = self.Updater (self.StartTime, t, CompactList.Unpack (self.ArgumentCount, self.Arguments))
-	return uncompleted == nil and true or uncompleted
+	
+	if uncompleted == false then
+		self.Completed = true
+	end
+	
+	return not self.Completed
 end
--- lua/carrier/packages/glass/animations/ianimation.lua
+++ lua/carrier/packages/glass/animations/ianimation.lua
@@ -8,5 +8,10 @@ function self:GetStartTime ()
 	Error ("IAnimation:GetStartTime : Not implemented.")
 end
 
+function self:IsCompleted ()
+	Error ("IAnimation:IsCompleted : Not implemented.")
+end
+
 function self:Update (t)
+	Error ("IAnimation:Update : Not implemented.")
 end
