34833a3c2cee099a248469475774bbddd69f3d76
notcake <cakenotfound@googlemail.com>
2017-09-29 04:39:15 +0100

    Added Pylon.Array and Pylon.String.

---
+++ lua/carrier/packages/pylon.array.lua
@@ -0,0 +1,47 @@
+Array = {}
+
+function Array.Concat (t, separator)
+	return table.concat (t, separator)
+end
+Array.concat = Array.Concat
+
+function Array.Filter (t, f)
+	local out = {}
+	for i = 1, #t do
+		if f (t [i]) then
+			out [#out + 1] = t [i]
+		end
+	end
+	return out
+end
+Array.filter = Array.Filter
+
+function Array.FlatMap (t, f)
+	local out = {}
+	for i = 1, #t do
+		local results = f (t [i])
+		for j = 1, #results do
+			out [#out + 1] = results [j]
+		end
+	end
+	return t
+end
+Array.flatMap = Array.FlatMap
+
+function Array.ForEach (t, f)
+	for i = 1, #t do
+		f (t [i])
+	end
+end
+Array.forEach = Array.ForEach
+
+function Array.Map (t, f)
+	local out = {}
+	for i = 1, #t do
+		out [i] = f (t [i])
+	end
+	return t
+end
+Array.map = Array.Map
+
+return Array
---
+++ lua/carrier/packages/pylon.string.lua
@@ -0,0 +1,27 @@
+String = {}
+
+local hexMap = {}
+for i = 0, 255 do
+	hexMap [string.format ("%02x", i)] = string.char (i)
+	hexMap [string.format ("%02X", i)] = string.char (i)
+end
+
+function String.FromHex (data)
+	local chars = {}
+	for hex in string.gmatch (data, "[0-9a-fA-F][0-9a-fA-F]") do
+		chars [#chars + 1] = hexMap [hex]
+	end
+	
+	return table.concat (chars)
+end
+
+function String.ToHex (str)
+	local hex = {}
+	for i = 1, #str do
+		hex [i] = string.format ("%02X", string.byte (i, i))
+	end
+	
+	return table.concat (hex, " ")
+end
+
+return String
