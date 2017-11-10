1e5bded77bd5115d64f61f3046af8bc0511464a7
notcake <cakenotfound@googlemail.com>
2017-11-10 13:55:41 +0000

    Fixed usage message for GarrysMod.UniqueIdCollider.App.

--- lua/carrier/packages/garrysmod.uniqueidcollider.app.lua
+++ lua/carrier/packages/garrysmod.uniqueidcollider.app.lua
@@ -2,11 +2,11 @@
 local UniqueIdCollider = require ("GarrysMod.UniqueIdCollider")
 
 if not arg or #arg == 0 then
-	print ("Usage: " .. arg [0] .. "<uniqueId>")
+	print ("Usage: " .. arg [0] .. " <uniqueId>")
 else
 	local uniqueId = tonumber (arg [1])
 	if not uniqueId then
-		print ("Usage: " .. arg [0] .. "<uniqueId>")
+		print ("Usage: " .. arg [0] .. " <uniqueId>")
 	else
 		local results = UniqueIdCollider (uniqueId)
 		print (string.format ("%d results for %s:", #results, arg [1]))
