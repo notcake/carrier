24267d12084ddc2655d3b781ea3f71b6069418a4
notcake <cakenotfound@googlemail.com>
2017-11-15 22:02:07 +0000

    Removed spacing from result of String.ToHex ().

--- lua/carrier/packages/pylon.string.lua
+++ lua/carrier/packages/pylon.string.lua
@@ -1,29 +1,31 @@
 -- PACKAGE Pylon.String
-
 String = {}
+
+local string_char   = string.char
+local string_format = string.format
+local string_gmatch = string.gmatch
+local string_gsub   = string.gsub
+local table_concat  = table.concat
 
 local hexMap = {}
 for i = 0, 255 do
-	hexMap [string.format ("%02x", i)] = string.char (i)
-	hexMap [string.format ("%02X", i)] = string.char (i)
+	hexMap [string_format ("%02x", i)] = string_char (i)
+	hexMap [string_format ("%02X", i)] = string_char (i)
 end
 
 function String.FromHex (data)
 	local chars = {}
-	for hex in string.gmatch (data, "[0-9a-fA-F][0-9a-fA-F]") do
+	for hex in string_gmatch (data, "[0-9a-fA-F][0-9a-fA-F]") do
 		chars [#chars + 1] = hexMap [hex]
 	end
 	
-	return table.concat (chars)
+	return table_concat (chars)
 end
 
+local hexMap = {}
+for i = 0, 255 do hexMap [string_char (i)] = string_format ("%02x", i) end
 function String.ToHex (str)
-	local hex = {}
-	for i = 1, #str do
-		hex [i] = string.format ("%02X", string.byte (i, i))
-	end
-	
-	return table.concat (hex, " ")
+	return string_gsub (str, ".", hexMap)
 end
 
 return String
