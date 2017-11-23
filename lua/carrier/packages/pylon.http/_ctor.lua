a131f8a7c13e08ef26fbd12d595109a87bb20fe1
notcake <cakenotfound@googlemail.com>
2017-11-23 03:11:21 +0000

    Added HTTP.EncodeUriComponent () and HTTP.DecodeUriComponent ().

--- lua/carrier/packages/pylon.http/_ctor.lua
+++ lua/carrier/packages/pylon.http/_ctor.lua
@@ -10,6 +10,22 @@ OOP.Initialize (_ENV)
 include ("httpcodes.lua")
 include ("httpresponse.lua")
 
+local encodeMap = {}
+local decodeMap = {}
+for i = 0, 255 do
+	encodeMap [string.char (i)] = string.format ("%%%02X", i)
+	decodeMap [string.format ("%%%02x", i)] = string.char (i)
+	decodeMap [string.format ("%%%02X", i)] = string.char (i)
+end
+
+function HTTP.EncodeUriComponent (s)
+	return string.gsub (s, "[^A-Za-z0-9%-_%.!~%*'%(%)]", encodeMap)
+end
+
+function HTTP.DecodeUriComponent (s)
+	return string.gsub (s, "%%[a-fA-F0-9][a-fA-F0-9]", decodeMap)
+end
+
 function HTTP.Get (url)
 	Error ("HTTP.Get : Not implemented.")
 end
@@ -21,11 +37,14 @@ end
 function HTTP.Initialize (destinationTable)
 	destinationTable = destinationTable or {}
 	
-	destinationTable.HTTPCodes    = HTTP.HTTPCodes
-	destinationTable.HTTPResponse = HTTP.HTTPResponse
+	destinationTable.HTTPCodes          = HTTP.HTTPCodes
+	destinationTable.HTTPResponse       = HTTP.HTTPResponse
 	
-	destinationTable.Get          = HTTP.Get
-	destinationTable.Post         = HTTP.Post
+	destinationTable.EncodeUriComponent = HTTP.EncodeUriComponent
+	destinationTable.DecodeUriComponent = HTTP.DecodeUriComponent
+	
+	destinationTable.Get                = HTTP.Get
+	destinationTable.Post               = HTTP.Post
 	
 	return destinationTable
 end
