f3e9a3005ec2830a529584ee0f50f123788e9e47
notcake <cakenotfound@googlemail.com>
2017-11-27 22:06:07 +0000

    Fixed HTTP requests not working on server startup.

--- lua/carrier/packages/pylon.http.garrysmod.lua
+++ lua/carrier/packages/pylon.http.garrysmod.lua
@@ -7,9 +7,42 @@ HTTP.Initialize (GarrysMod)
 
 local Future = require ("Pylon.Future")
 
+-- Workaround for HTTP failed - ISteamHTTP isn't available!
+local ready = CurTime () > 0
+local requestQueue = nil
+
+local function DispatchRequest (f, ...)
+	if ready then f (...) return end
+	
+	if requestQueue == nil then
+		requestQueue = {}
+		
+		hook.Add ("Tick", "Pylon.HTTP.GarrysMod",
+			function ()
+				hook.Remove ("Tick", "Pylon.HTTP.GarrysMod")
+				ready = true
+				
+				for i = 1, #requestQueue do
+					requestQueue [i] ()
+				end
+				
+				requestQueue = nil
+			end
+		)
+	end
+	
+	local arguments = {...}
+	local argumentCount = select ("#", ...)
+	requestQueue [#requestQueue + 1] = function ()
+		f (unpack (arguments, 1, argumentCount))
+	end
+end
+
 function GarrysMod.Get (url)
 	local future = Future ()
-	http.Fetch (url,
+	DispatchRequest (
+		http.Fetch,
+		url,
 		function (content, contentLength, headers, code)
 			local response = HTTP.HTTPResponse.FromHTTPResponse (url, code, content, headers)
 			future:Resolve (response)
@@ -24,7 +57,9 @@ end
 
 function GarrysMod.Post (url, parameters)
 	local future = Future ()
-	http.Post (url, parameters,
+	DispatchRequest (
+		http.Post,
+		url, parameters,
 		function (content, contentLength, headers, code)
 			local response = HTTP.HTTPResponse.FromHTTPResponse (url, code, content, headers)
 			future:Resolve (response)
