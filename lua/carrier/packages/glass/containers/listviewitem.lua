44e9e898f2bdbecd0e48caad802aa162b7ad0e48
notcake <cakenotfound@googlemail.com>
2017-11-27 22:05:42 +0000

    Cleaned up ListViewItem hover logic.

--- lua/carrier/packages/glass/containers/listviewitem.lua
+++ lua/carrier/packages/glass/containers/listviewitem.lua
@@ -2,21 +2,12 @@ local self = {}
 Glass.ListViewItem = Class (self, Glass.View)
 
 function self:ctor ()
-	self.Hovered = false
 end
 
 -- IView
 -- Internal
-function self:OnMouseEnter ()
-	self.Hovered = true
-end
-
-function self:OnMouseLeave ()
-	self.Hovered = false
-end
-
 function self:Render (w, h, render2d)
-	if self.Hovered then
+	if self:IsMouseOver () then
 		render2d:FillRectangle (Color.WithAlpha (Color.LightBlue, 192), 0, 0, w, h)
 	end
 end
