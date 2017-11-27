279aa2884a0319e6d24572f8de6311b5b89e9c30
notcake <cakenotfound@googlemail.com>
2017-11-27 22:06:34 +0000

    Fixed bootstrap HTTP requests not working on server startup.

--- lua/carrier/packages/carrier.bootstrap/carrier.bootstrap.lua
+++ lua/carrier/packages/carrier.bootstrap/carrier.bootstrap.lua
@@ -1131,10 +1131,41 @@ local GarrysMod = {}
 
 HTTP.Initialize (GarrysMod)
 
+local ready = CurTime () > 0
+local requestQueue = nil
+
+local function DispatchRequest (f, ...)
+	if ready then f (...) return end
+	
+	if requestQueue == nil then
+		requestQueue = {}
+		
+		hook.Add ("Tick", "Pylon.HTTP.GarrysMod",
+			function ()
+				hook.Remove ("Tick", "Pylon.HTTP.GarrysMod")
+				ready = true
+				
+				for i = 1, #requestQueue do
+					requestQueue [i] ()
+				end
+				
+				requestQueue = nil
+			end
+		)
+	end
+	
+	local arguments = {...}
+	local argumentCount = select ("#", ...)
+	requestQueue [#requestQueue + 1] = function ()
+		f (unpack (arguments, 1, argumentCount))
+	end
+end
 
 function GarrysMod.Get (url)
 	local future = Future ()
-	http.Fetch (url,
+	DispatchRequest (
+		http.Fetch,
+		url,
 		function (content, contentLength, headers, code)
 			local response = HTTP.HTTPResponse.FromHTTPResponse (url, code, content, headers)
 			future:Resolve (response)
@@ -1149,7 +1180,9 @@ end
 
 function GarrysMod.Post (url, parameters)
 	local future = Future ()
-	http.Post (url, parameters,
+	DispatchRequest (
+		http.Post,
+		url, parameters,
 		function (content, contentLength, headers, code)
 			local response = HTTP.HTTPResponse.FromHTTPResponse (url, code, content, headers)
 			future:Resolve (response)
