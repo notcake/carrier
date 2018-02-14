4eedcf74166920549d35695f6067bddf8d49fcfc
notcake <cakenotfound@googlemail.com>
2018-02-14 21:30:40 +0000

    Implemented monotonic clock using gettimeofday and ffi under LuaJIT.

---
+++ lua/carrier/packages/pylon.monotonicclock.luajit.lua
@@ -0,0 +1,24 @@
+-- PACKAGE Pylon.MonotonicClock.LuaJIT
+
+local ffi = require ("ffi")
+
+ffi.cdef ("typedef long time_t;")
+
+ffi.cdef (
+	[[
+		typedef struct timeval {
+			time_t tv_sec;
+			time_t tv_usec;
+		} timeval;
+	]]
+)
+
+ffi.cdef ("int gettimeofday(struct timeval *t, void *tzp);")
+
+return function ()
+	local t = ffi.new ("struct timeval")
+	local ret = ffi.C.gettimeofday (t, nil)
+	if ret ~= 0 then error () end
+	
+	return tonumber (t.tv_sec) + tonumber (t.tv_usec) / 1000000
+end
