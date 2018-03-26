af237d3132cd9f335cd1770bd86b46b7bee1b366
notcake <cakenotfound@googlemail.com>
2018-03-26 02:46:34 +0100

    Added more set functions.

--- lua/carrier/packages/pylon.set.lua
+++ lua/carrier/packages/pylon.set.lua
@@ -2,8 +2,8 @@
 
 Set = {}
 
-function Set.Copy (set)
-	local out = {}
+function Set.Copy (set, out)
+	local out = out or {}
 	for k, _ in pairs (set) do
 		out [k] = set [k]
 	end
@@ -12,6 +12,59 @@ end
 Set.copy  = Set.Copy
 Set.clone = Set.copy
 Set.Clone = Set.Copy
+
+function Set.Intersect (a, b, out)
+	local out = out or {}
+	for k, _ in pairs (a) do
+		if b [k] then
+			out [k] = true
+		end
+	end
+	
+	return out
+end
+Set.intersect = Set.Intersect
+
+function Set.IntersectArray (array)
+	if #array == 0 then return {} end
+	
+	local out = Set.Copy (array [1])
+	
+	for i = 2, #array do
+		for k, _ in pairs (out) do
+			if not array [i] [k] then
+				out [k] = nil
+			end
+		end
+	end
+	
+	return out
+end
+
+function Set.Union (a, b, out)
+	local out = out or {}
+	for k, _ in pairs (a) do
+		out [k] = true
+	end
+	for k, _ in pairs (b) do
+		out [k] = true
+	end
+	
+	return out
+end
+Set.union = Set.Union
+
+function Set.UnionArray (array, out)
+	local out = out or {}
+	
+	for i = 1, #array do
+		for k, _ in pairs (array [i]) do
+			out [k] = true
+		end
+	end
+	
+	return out
+end
 
 function Set.Filter (set, f)
 	local out = {}
